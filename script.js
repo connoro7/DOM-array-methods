345fa62e28a1705ba479a39f41bc7dc57f610235
Connor Dillon <connor@codewithconnor.com>
2020-11-19 14:44:25 -0800

    :tada: Added DOM interactivity, currency formatting

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const sortBtn = document.getElementById('sort')
 const calculateWealthBtn = document.getElementById('calculate-wealth')
 
 let data = []
+getRandomUser()
+getRandomUser()
+getRandomUser()
 
 // Fetch random user and add some ca$h
 async function getRandomUser() {
@@ -24,7 +27,28 @@ async function getRandomUser() {
 // Add new object to data array
 function addData(object) {
   data.push(object)
+
+  updateDOM()
 }
-getRandomUser()
-getRandomUser()
-getRandomUser()
+
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>'
+  providedData.forEach((item, index, arr) => {
+    const element = document.createElement('div')
+    element.classList.add('person')
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`
+    // Add new element to DOM
+    main.appendChild(element)
+  })
+}
+
+// Format number as money
+// Reference: https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
+}
+
+// Event Listeners
+// Add user
+addUserBtn.addEventListener('click', getRandomUser) 
